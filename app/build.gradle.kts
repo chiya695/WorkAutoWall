a9888d51b350ea98a3879c012f94af04d4f60897
Chiya <80669547+chiya695@users.noreply.github.com>
2026-05-17 10:19:44 +0800

    chore: bump version to 1.0.3 (code=3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.chiya.workautowall"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.3"
     }
 
     signingConfigs {
